76dee4cd601f4f7acdac6df1cedd8d647a8b2ea2
Gunnar Morling <gunnar.morling@googlemail.com>
2018-09-26 14:55:39 +0200

    Embedding KStreams into Swarm

--- kstreams-live-update/temperature-aggregator/src/main/java/io/debezium/examples/kstreams/liveupdate/aggregator/ws/ChangeEventsWebsocketEndpoint.java
+++ kstreams-live-update/temperature-aggregator/src/main/java/io/debezium/examples/kstreams/liveupdate/aggregator/ws/ChangeEventsWebsocketEndpoint.java
@@ -1,8 +1,39 @@
-package io.debezium.examples.kstreams.liveupdate.aggregator;
+/**
+ *  Copyright 2018 Gunnar Morling (http://www.gunnarmorling.de/). See
+ *  the copyright.txt file in the distribution for a full listing of all
+ *  contributors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package io.debezium.examples.kstreams.liveupdate.aggregator.ws;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.Collections;
 import java.util.Properties;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Logger;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.enterprise.context.ApplicationScoped;
+import javax.websocket.CloseReason;
+import javax.websocket.OnClose;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
 
 import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
@@ -13,6 +44,7 @@ import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.Joined;
+import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.kstream.Printed;
@@ -27,19 +59,19 @@ import io.debezium.examples.kstreams.liveupdate.aggregator.serdes.LongKeySerde;
 import io.debezium.examples.kstreams.liveupdate.aggregator.serdes.StationSerde;
 import io.debezium.examples.kstreams.liveupdate.aggregator.serdes.TemperatureMeasurementSerde;
 
-public class Main {
+@ServerEndpoint("/example")
+@ApplicationScoped
+public class ChangeEventsWebsocketEndpoint {
 
-    public static void main(String[] args) {
+    Logger log = Logger.getLogger( this.getClass().getName() );
 
-        if(args.length != 3) {
-            System.err.println("usage: java -jar <package> "
-                    + Main.class.getName() + " <parent_topic> <children_topic> <bootstrap_servers>");
-            System.exit(-1);
-        }
+    private final Set<Session> sessions = Collections.newSetFromMap( new ConcurrentHashMap<>() );
 
-        final String parentTopic = args[0];
-        final String childrenTopic = args[1];
-        final String bootstrapServers = args[2];
+    private KafkaStreams streams;
+
+    @PostConstruct
+    public void startKStreams() {
+        final String bootstrapServers = "kafka:9092";
 
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streaming-aggregates-ddd");
@@ -59,7 +91,7 @@ public class Main {
 
         KTable<Long, Station> stations = builder.table("dbserver1.inventory.stations", Consumed.with(longKeySerde, stationSerde));
 
-        KTable<String, String> maxTemperaturesByStation = builder.stream(
+        KStream<String, String> maxTemperaturesByStation = builder.stream(
                 "dbserver1.inventory.temperature_measurements",
                 Consumed.with(longKeySerde, temperatureMeasurementsSerde)
             )
@@ -88,15 +120,40 @@ public class Main {
                 avg = avg.setScale(1, RoundingMode.HALF_UP);
                 return avg.doubleValue();
             })
-            .mapValues(String::valueOf);
+            .mapValues(String::valueOf)
+            .toStream()
+            .peek((k, v) -> sessions.forEach(s -> {
+                try {
+                    s.getBasicRemote().sendText( "{ \"station\" : \"" + k + "\", \"average-temperature\" : \"" + v + "\" }" );
+                }
+                catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }));
 
+        maxTemperaturesByStation.to("average_temperatures_by_station", Produced.with(Serdes.String(), Serdes.String()));
+        maxTemperaturesByStation.print(Printed.toSysOut());
 
-        maxTemperaturesByStation.toStream().to("average_temperatures_by_station", Produced.with(Serdes.String(), Serdes.String()));
-        maxTemperaturesByStation.toStream().print(Printed.toSysOut());
-
-        final KafkaStreams streams = new KafkaStreams(builder.build(), props);
+        streams = new KafkaStreams(builder.build(), props);
         Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
 
         streams.start();
+    }
+
+    @PreDestroy
+    public void closeKStreams() {
+        streams.close();
+    }
+
+    @OnOpen
+    public void open(Session session) {
+        log.info( "Opening session: " + session.getId() );
+        sessions.add(session);
+    }
+
+    @OnClose
+    public void close(Session session, CloseReason c) {
+        sessions.remove( session );
+        log.info( "Closing: " + session.getId() );
     }
 }
