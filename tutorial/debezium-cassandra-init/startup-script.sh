798ff68b27897a4582b03e28429e50e1b13617c9
Jiri Pechanec <jpechane@redhat.com>
2020-12-04 07:16:24 +0100

    Debezium start

--- tutorial/debezium-cassandra-init/startup-script.sh
+++ tutorial/debezium-cassandra-init/startup-script.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-sudo sh /opt/cassandra/bin/cassandra -f
+sh /opt/cassandra/bin/cassandra -f &
 
-sed -i 's/^#cdc_raw_directory/cdc_raw_directory/g' /opt/cassandra/conf/cassandra.yaml
-sed -i 's/^#cdc_enabled/cdc_enabled/g' /opt/cassandra/conf/cassandra.yaml
+sleep 20
 
-java -Dlog4j.debug -Dlog4j.configuration=file:/tmp/log4j.properties -jar /tmp/debezium-connector-cassandra.jar /tmp/config.properties
+java -Dlog4j.debug -Dlog4j.configuration=file:$DEBEZIUM_HOME/log4j.properties -jar $DEBEZIUM_HOME/debezium-connector-cassandra.jar $DEBEZIUM_HOME/config.properties
